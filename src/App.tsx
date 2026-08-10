2961ddef386dcca713bf4d350ac018f53a37b88f
sergio guerra <isaac7Guerra@gmail.com>
2026-08-10 11:08:48 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div >
+      <form>
+        <label>Ingrese su nombre</label>
+        <input type="text" placeholder='ingrese su nombre' required></input>
+      </form>
+    </div>
+    </>
+  )
+}
+
+export default App
